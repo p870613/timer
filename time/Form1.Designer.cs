caff8bf56529197597302c0f912e01d8ba0a13ac
p870613 <p870613@yahoo.com.tw>
2019-09-30 23:50:12 +0800

    timer

---
+++ time/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace time
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bottom1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bottom1
+            // 
+            this.bottom1.Location = new System.Drawing.Point(10, 77);
+            this.bottom1.Name = "bottom1";
+            this.bottom1.Size = new System.Drawing.Size(75, 23);
+            this.bottom1.TabIndex = 0;
+            this.bottom1.Text = "開始";
+            this.bottom1.UseVisualStyleBackColor = true;
+            this.bottom1.Click += new System.EventHandler(this.bottom1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "重設";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(91, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "停止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(77, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 115);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bottom1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "計時器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bottom1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ time/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace time
+{
+    public partial class Form1 : Form
+    {
+        int count=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            count++;
+            label1.Text = "" + count;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+        private void bottom1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            string s = label1.Text;
+            
+            count = (s[7] - '0' + (s[6] - '0') * 10)
+                    +  (60 * (s[4] - '0' + (s[3] - '0') * 10))
+                    + (3600 * (s[1] - '0' + (s[0] - '0') * 10));
+        }
+        /*01:34:67*/
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "00:00:00";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 1000;   
+            count++;
+
+            label1.Text = ((count / 3600).ToString()).PadLeft(2, '0')+":" +((count / 60 % 60).ToString()).PadLeft(2, '0') + ":"+ ((count % 60).ToString()).PadLeft(2, '0');
+                       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Icon = new System.Drawing.Icon(@"4.ico");
+        }
+    }
+}
